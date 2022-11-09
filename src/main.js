802c5dad7ca7b14d6b0ba83ccf0edea1f30c862a
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-11-09 15:14:36 +0100

    Hosted on Vercel

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import "./style.css";
+import './style.css';
 const APP_URL = import.meta.env.VITE_APP_URL;
 
 document.addEventListener("DOMContentLoaded", () => {
-	if (window.localStorage.getItem("acessToken")) {
+	if (localStorage.getItem("accessToken")) {
 		window.location.href = `${APP_URL}/dashboard/dashboard.html`;
 	} else {
 		window.location.href = `${APP_URL}/login/login.html`;
 	}
-});
+})
+
